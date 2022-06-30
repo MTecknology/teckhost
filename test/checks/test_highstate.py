a49a733cd4b7ac2f50bed4f119b54aa0f3a1c94a
Michael Lustfield <michael@lustfield.net>
2022-06-30 00:03:41 -0500

    Fix up a pylint mistake

--- test/checks/test_highstate.py
+++ test/checks/test_highstate.py
@@ -10,7 +10,7 @@ from helpers import please
 @pytest.mark.admin
 def test_two_highstates(host):
     '''Verify a highstate can complete with no errors and a second run produces no changes'''
-    for run in range(1,2):
+    for run in range(1, 2):
         highstate = please(host, 'salt-call --local --state-verbose=False state.highstate')
         assert highstate.succeeded
         assert 'Failed:0' in highstate.stdout.replace(' ', '')
